d393ea790337c7668b5b9d8be49be4c6119d9c37
SebaAguiar <sebaaguiar08@gmail.com>
2026-06-30 20:14:34 -0300

    feat(common): create package and define shared typescript utilities
    Establishes the  package to hold runtime-agnostic types
    and shared interfaces such as , , and a robust
     utility to avoid circular dependencies between packages.

---
+++ packages/common/src/types.ts
@@ -0,0 +1,33 @@
+export interface Constructor<T = object> {
+  new (...args: never[]): T;
+}
+
+export type Token<T = object> = Constructor<T> | symbol | string;
+
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Map<infer K, infer V>
+  ? Map<DeepPartial<K>, DeepPartial<V>>
+  : T extends ReadonlyMap<infer K, infer V>
+  ? ReadonlyMap<DeepPartial<K>, DeepPartial<V>>
+  : T extends WeakMap<infer K, infer V>
+  ? WeakMap<DeepPartial<K>, DeepPartial<V>>
+  : T extends Set<infer U>
+  ? Set<DeepPartial<U>>
+  : T extends ReadonlySet<infer U>
+  ? ReadonlySet<DeepPartial<U>>
+  : T extends WeakSet<infer U>
+  ? WeakSet<DeepPartial<U>>
+  : T extends Array<infer U>
+  ? T extends IsTuple<T>
+    ? { [K in keyof T]: DeepPartial<T[K]> }
+    : Array<DeepPartial<U>>
+  : T extends Promise<infer U>
+  ? Promise<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+type Builtin = Function | Date | Error | RegExp;
+
+type IsTuple<T> = T extends readonly any[] ? (number extends T['length'] ? never : T) : never;
